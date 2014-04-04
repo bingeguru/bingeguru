5cf871d4993a55c6902fd046434f09735bbb8998
Hack Reactor Students <accounts@hackreactor.com>
2014-04-04 16:17:47 -0700

    Adds views for find/discover and title links back to main view.

--- public/client/angApp.js
+++ public/client/angApp.js
@@ -2,10 +2,6 @@ var angApp = angular.module('angApp', [
   'ngRoute', 'appControllers'
 ]);
 
-
-
-
-
 angApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider
@@ -13,8 +9,15 @@ angApp.config(['$routeProvider',
         templateUrl: 'client/views/home.html',
         controller: 'homeCtrl'
       })
+      .when('/discover', {
+        templateUrl: 'client/views/discover.html',
+        controller: 'discoverCtrl'
+      })
+      .when('/find', {
+        templateUrl: 'client/views/find.html',
+        controller: 'findCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
   }]);
-
--- public/client/appControllers.js
+++ public/client/appControllers.js
@@ -2,3 +2,9 @@ var appControllers = angular.module('appControllers', []);
 
 appControllers.controller('homeCtrl', ['$scope', function($scope){
 }]);
+
+appControllers.controller('discoverCtrl', ['$scope', function($scope){
+}]);
+
+appControllers.controller('findCtrl', ['$scope', function($scope){
+}]);
