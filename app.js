2719b973ae1a0f42f5b2ac89458c0860842b9a66
Hack Reactor Students <accounts@hackreactor.com>
2014-04-04 17:59:32 -0700

    Data from trakt api into shows array

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var trakt = require('node-trakt');
+var password = require('./password');
 
 var app = express();
 
@@ -28,8 +30,57 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+//ROUTES
 app.get('/', routes.index);
-app.get('/users', user.list);
+
+//TRAKT API CONNECTION
+trakt.init(password.api);
+
+//SHOW LIST
+
+var showList = [
+    {name:"The Wire", title:"the-wire"},
+    {name:"The West Wing", title: "the-west-wing"},
+    {name: "The Office", title: "the-office-us"},
+    {name: "Friday Night Lights", title: "friday-night-lights"},
+    {name: "My So-Called Life", title: "my-so-called-life" },
+    {name: "Breaking Bad", title: "my-so-called-life"},
+    {name: "Game of Thrones", title: "game-of-thrones"},
+    {name: "Parks and Recreation", title:"parks-and-recreation"},
+    {name: "The Good Wife", title: "the-good-wife"},
+    {name: "Freaks and Geeks", title:"freaks-and-geeks"}
+  ];
+
+//Array of all SHOWS
+var shows = [];
+
+// GET DATA
+trakt.login(password.username, password.password , function(){
+  for(var i = 0; i<showList.length; i++){
+
+      trakt.showSeasons({title : showList[i].title},function(err, data){
+        if(err){
+          throw err;
+        }
+        var show = {seasons:{}};
+        for(var j = 0; j < data.length; j++){
+          var showName = data[j].url;
+          showName = showName.split('/');
+          show['title'] = showName[4];
+          show.seasons[data[j].season+1] = data[j].episodes;
+
+
+        console.log("show", show);
+        }
+      });
+        // console.log(showList[i],shows);
+
+  }
+ // console.log(shows);
+
+}); // trakt login
+
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
