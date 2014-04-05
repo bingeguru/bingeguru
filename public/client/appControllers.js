129e906773019a058bac55b29b4dae3b95530c9b
Hack Reactor Students <students@hackreactor.com>
2014-04-05 12:16:55 -0700

    Triggers get request upon clicking into discover page.

--- public/client/appControllers.js
+++ public/client/appControllers.js
@@ -1,12 +1,13 @@
 var appControllers = angular.module('appControllers', ['testData']);
 
 appControllers.controller('homeCtrl', ['$scope', function($scope){
+
 }]);
 
 appControllers.controller('findCtrl', ['$scope', function($scope){
 }]);
 
-appControllers.controller('discoverCtrl', ['$scope', 'testDataFactory', function($scope, testDataFactory){
+appControllers.controller('discoverCtrl', ['$scope', 'testDataFactory', '$http', function($scope, testDataFactory, $http){
    $scope.data = testDataFactory.shows;
    for (var i = 0; i < $scope.data.length; i++) {
      var seasonList =$scope.data[i].seasons;
@@ -15,11 +16,20 @@ appControllers.controller('discoverCtrl', ['$scope', 'testDataFactory', function
      for(var episode in seasonList){
        totalEp += seasonList[episode];
      }
-     
+
      $scope.data[i]['totalSeasons'] = totalSeasons;
      $scope.data[i]['totalEp'] = totalEp;
    };
    console.log($scope.data);
 
+  $scope.requestShowData = function(){
+    $http({method: 'GET', url: '/getShows'})
+      .success(function(data, status, headers, config) {
+        console.log('get success')
+      })
+      .error(function(data, status, headers, config){
+        console.log('get error');
+      });
+   }
 
 }]);
