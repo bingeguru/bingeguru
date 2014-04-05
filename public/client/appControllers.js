fbd794f8752b03075151a1468a2a3e97e46b6b9d
Hack Reactor Students <students@hackreactor.com>
2014-04-05 10:55:26 -0700

    Renders table of show data and computes total number of season and episodes.

--- public/client/appControllers.js
+++ public/client/appControllers.js
@@ -8,5 +8,18 @@ appControllers.controller('discoverCtrl', ['$scope', function($scope){
 
 appControllers.controller('findCtrl', ['$scope', 'testDataFactory', function($scope, testDataFactory){
    $scope.data = testDataFactory.shows;
+   for (var i = 0; i < $scope.data.length; i++) {
+     var seasonList =$scope.data[i].seasons;
+     var totalSeasons = Object.keys(seasonList).length
+     var totalEp = 0;
+     for(var episode in seasonList){
+       totalEp += seasonList[episode];
+     }
+     
+     $scope.data[i]['totalSeasons'] = totalSeasons;
+     $scope.data[i]['totalEp'] = totalEp;
+   };
    console.log($scope.data);
+
+
 }]);
