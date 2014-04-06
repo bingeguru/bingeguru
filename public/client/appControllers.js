33f6c0642c2ad6c74405d3f575ba4221e3940b75
Hack Reactor Students <github@hackreactor.com>
2014-04-05 19:33:22 -0700

    Hooked up client to local server

--- public/client/appControllers.js
+++ public/client/appControllers.js
@@ -10,24 +10,36 @@ appControllers.controller('findCtrl', ['$scope', '$http', function($scope, $http
 }]);
 
 appControllers.controller('discoverCtrl', ['$scope', 'testDataFactory', '$http', function($scope, testDataFactory, $http){
-   $scope.data = testDataFactory.shows;
-   for (var i = 0; i < $scope.data.length; i++) {
-     var seasonList =$scope.data[i].seasons;
-     var totalSeasons = Object.keys(seasonList).length
-     var totalEp = 0;
-     for(var episode in seasonList){
-       totalEp += seasonList[episode];
+   // $scope.data = testDataFactory.shows;
+   // for (var i = 0; i < $scope.data.length; i++) {
+   //   var seasonList =$scope.data[i].seasons;
+   //   var totalSeasons = Object.keys(seasonList).length
+   //   var totalEp = 0;
+   //   for(var episode in seasonList){
+   //     totalEp += seasonList[episode];
+   //   }
+
+   //   $scope.data[i]['totalSeasons'] = totalSeasons;
+   //   $scope.data[i]['totalEp'] = totalEp;
+   // };
+   // console.log($scope.data);
+
+  $scope.requestShowData = function(){
+    $http({method: 'GET', url: '/getShows'})
+      .success(function(data, status, headers, config) {
+        console.log(data);
+        $scope.data = data;
+         for (var i = 0; i < $scope.data.length; i++) {
+           var seasonList =$scope.data[i].seasons;
+           var totalSeasons = Object.keys(seasonList).length
+           var totalEp = 0;
+           for(var episode in seasonList){
+             totalEp += seasonList[episode];
      }
 
      $scope.data[i]['totalSeasons'] = totalSeasons;
      $scope.data[i]['totalEp'] = totalEp;
    };
-   console.log($scope.data);
-
-  $scope.requestShowData = function(){
-    $http({method: 'GET', url: '/getShows'})
-      .success(function(data, status, headers, config) {
-        console.log('get success')
       })
       .error(function(data, status, headers, config){
         console.log('get error');
