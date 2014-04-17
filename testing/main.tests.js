34e9f6aa804ce5d1abbd4279dca633c2b6a12e66
lexjacobs <lex.jacobs@gmail.com>
2014-04-17 11:58:58 -0700

    removes testing from pushed version.

--- testing/main.tests.js
+++
@@ -1,5 +0,0 @@
-describe('Aspecsuite',function(){ it('contains a passing spec', function() {
-expect(true).toBe(true); });
-it('contains another passing spec', function() { expect(false).not.toBe(true);
-}); });
-
