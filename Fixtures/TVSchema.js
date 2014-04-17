352fee8b906c86cc9729aa6bb73329b0405affa3
Gulsen Yilmaz <gsenol@gmail.com>
2014-04-16 17:04:46 -0700

    updated database to have 250 shows and additional information - db is local

--- Fixtures/TVSchema.js
+++ Fixtures/TVSchema.js
@@ -5,32 +5,40 @@ var TVSchema = new mongoose.Schema({
   name: String,
   title: String,
   runtime: Number,
-  seasons: [],
-  poster: String,
   totalTime: Number,
-  genres: []
+  poster: String,
+  imdb_rating: Number,
+  ratings: Number,
+  votes: Number,
+  year: Number,
+  network: String,
+  overview: String,
+  genres: [],
+  netflix: Boolean,
+  hulu: Boolean,
+  amazon: Boolean,
+  itunes: Boolean,
+  seasons:[],
+  seasons_details: [{
+  	season: Number,
+  	episodes: Number,
+    url: String,
+    poster: String,
+    images: {
+      poster:String
+    }
+  }],
+  episodes: [{
+    season: Number,
+    episode: Number,
+    title: String,
+    overview: String
+  }],
+  people: [{
+  	name: String,
+  	character: String,
+  	images: String
+  }]
 });
 
-// TVSchema.instance.processData = function(data){
-//  //tkaes each instance from the database
-//  //makes api call
-//  //saves to db
-// };
-
 module.exports = mongoose.model('tvshow', TVSchema);
-
-// trakt.showSeasons({title : showList[i].title},function(err, data){
-//         if(err){
-//           throw err;
-//         }
-//         var show = {seasons:{}};
-//         for(var j = 0; j < data.length; j++){
-//           var showName = data[j].url;
-//           showName = showName.split('/');
-//           show['title'] = showName[4];
-//           show.seasons[data[j].season+1] = data[j].episodes;
-//         }
-//         collection.insert(show, {w:1}, function(err,result) {
-//           console.log(result);
-//         });
-//       });
--- fetchAPI.js
+++ fetchAPI.js
@@ -1,41 +1,76 @@
+var trakt = require('node-trakt');
 var password = require('./Fixtures/password');
 var showlist = require('./Fixtures/showList');
 var tvSchema = require('./Fixtures/TVSchema');
-var trakt = require('node-trakt');
-var _ = require('underscore');
+var db = require('mongoose');
+var TVShows = db.model('tvshow');
 
-//TRAKT API CONNECTION
+// checking db entries
+TVShows.find({title: 'the-newsroom'}, function(err, shows){
+  shows.forEach(function(show){
+  // 	show.episodes = [];
+  // 	show.save(function(err, result){
+  // 		if(err) console.log(err);
+  	// });
+    console.log(show.name, show.episodes);
+  });
+});
 
-// var newShow = new tvSchema({
-//   name:
-// });
+trakt.init(password.api);
+trakt.login(password.username, password.password , function(){
+  console.log("API call!");
+  TVShows.find({}, function (err, shows) {
+  	shows.forEach(function(show){
 
-// newShow.save(function(err, show){
+  		//populating related info
+  		// trakt.showSummary({title: show.title}, function(err,data){
+  		// 	show.year = data.year;
+  		// 	show.overview = data.overview;
+  		// 	show.runtime = data.runtime;
+  		// 	show.network = data.network;
+  		// 	show.poster = data.poster;
+  		// 	show.ratings = (data.ratings) ? data.ratings.percentage : 0;
+  		// 	show.votes = (data.ratings) ? data.ratings.votes : 0;
+  		// 	show.genres = data.genres;
+  		// 	show.people = (data.people) ? data.people.actors : "";
+  		// 	show.save(function(err, result){
+  		// 		if(err) console.log('error in saving to db', err);
+  		// 	});
+  		// });
 
-// })
+  		// // populating season details
+  		// trakt.showSeasons({title: show.title}, function(err, data){
+  		// 	show.seasons_details = data;
+  		// 	if(Array.isArray(data) && data.length > 0){	
+  		// 		data.forEach(function(item){
+	  	// 			show.seasons.push([item.season, item.episodes]);
+	  	// 		});
+  		// 	}
+  		// 	show.save(function(err, result){
+  		// 		if(err) console.log('error in saving to db', err);
+	   //      });
+	   //  });
 
+    	// // populating episode details
+    	show.seasons.forEach(function(season){
+    		if(season.length > 0){
+		        trakt.showSeason({title: show.title, season: season}, function(err, data){
+		        	if(Array.isArray(data) && data.length > 0){
+			        	data.forEach(function(episode){
+			        		show.episodes.push({
+			        			season: episode.season,
+			        			episode: episode.episode,
+			        			title: episode.title,
+			        			overview: episode.overview});
+			        	});
+		        	}
+		        	show.save(function(err, result){
+		        		if(err) console.log('error in saving to db', err);
+		        	});
+		        });
+    		}
+	    });
 
-// var fetchAPI = function(){
-//   trakt.init(password.api);
-//   trakt.login(password.username, password.password , function(){
-//     console.log("HERE");
-//     tvSchema.find({}, function (err, shows) {
-//       shows.forEach(function(show){
-//         trakt.showSeasons({title: 'the-wire'}, function(err, data){
-//           _.each(data, function(obj){
-//           show.seasons.push([obj.season, obj.episodes]);
-//           });
-//           show.save(function(err,result){
-//             if (err) return console.error(err);
-//           });
-//         });
-//       });
-//     });
-
-//   });
-// };
-
-// fetchAPI();
-
-// module.exports = fetchAPI;
-
+    });
+  });
+});
