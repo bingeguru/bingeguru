72d38103c9e2dc6c91c6ea09be2276c73af8640c
tinatinatina <cunningham.cgr@gmail.com>
2014-04-17 15:05:10 -0700

    detail page and modal layout

--- public/client/appControllers.js
+++ public/client/appControllers.js
@@ -31,10 +31,10 @@ appControllers.controller('errCtrl', ['$scope', '$location', 'getFiltered' ,'get
 
        $scope.data[i]['totalSeasons'] = totalSeasons;
        $scope.data[i]['totalEp'] = totalEp;
-       $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) / 60);
+       $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']* $scope.data[i]['totalSeasons']) / 60);
        $scope.data[i]['bingeMins'] = ($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) % 60;
-       $scope.data[i]['bingeWeeks'] = Math.floor($scope.data[i]['totalEp'] / 7);
-       $scope.data[i]['bingeDays'] = $scope.data[i]['totalEp'] % 7;
+       $scope.data[i]['bingeWeeks'] = Math.floor(($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) / 7);
+       $scope.data[i]['bingeDays'] = ($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) % 7;
      }
     })
     .error(function(data, status, headers, config){
@@ -124,12 +124,12 @@ appControllers.controller('ButtonsCtrl', ['$scope', '$location', 'getFiltered' ,
         totalEp += parseInt(seasonList[episode], 10);
        }
 
-       $scope.data[i]['totalSeasons'] = totalSeasons;
-       $scope.data[i]['totalEp'] = totalEp;
-       $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) / 60);
-       $scope.data[i]['bingeMins'] = ($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) % 60;
-       $scope.data[i]['bingeWeeks'] = Math.floor($scope.data[i]['totalEp'] / 7);
-       $scope.data[i]['bingeDays'] = $scope.data[i]['totalEp'] % 7;
+         $scope.data[i]['totalSeasons'] = totalSeasons;
+         $scope.data[i]['totalEp'] = totalEp;
+         $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']* $scope.data[i]['totalSeasons']) / 60);
+         $scope.data[i]['bingeMins'] = ($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) % 60;
+         $scope.data[i]['bingeWeeks'] = Math.floor(($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) / 7);
+         $scope.data[i]['bingeDays'] = ($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) % 7;
      }
     })
     .error(function(data, status, headers, config){
@@ -187,10 +187,11 @@ appControllers.controller('showDetailCtrl', ['$scope', '$location', 'getFiltered
 
        $scope.data['totalSeasons'] = totalSeasons;
        $scope.data['totalEp'] = totalEp;
-       $scope.data['bingeHours'] = Math.floor(($scope.data['totalEp'] * $scope.data['runtime']) / 60);
-       $scope.data['bingeMins'] = ($scope.data['totalEp'] * $scope.data['runtime']) % 60;
-       $scope.data['bingeWeeks'] = Math.floor($scope.data['totalEp'] / 7);
-       $scope.data['bingeDays'] = $scope.data['totalEp'] % 7;
+       $scope.data['totaltime'] = ($scope.data['totalEp'] * $scope.data['runtime'] * $scope.data['totalSeasons']);
+       $scope.data['bingeHours'] = Math.floor(($scope.data['totaltime'] / 60));
+       $scope.data['bingeMins'] = ($scope.data['totalEp'] * $scope.data['runtime']* $scope.data['totalSeasons']) % 60;
+       $scope.data['bingeWeeks'] = Math.floor(($scope.data['totalEp'] * $scope.data['totalSeasons'])/ 7);
+       $scope.data['bingeDays'] = ($scope.data['totalEp'] * $scope.data['totalSeasons']) % 7;
     }
    };
 
@@ -241,10 +242,10 @@ appControllers.controller('discoverCtrl', ['$scope', '$http', 'getFiltered', '$l
 
          $scope.data[i]['totalSeasons'] = totalSeasons;
          $scope.data[i]['totalEp'] = totalEp;
-         $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) / 60);
+         $scope.data[i]['bingeHours'] = Math.floor(($scope.data[i]['totalEp'] * $scope.data[i]['runtime']* $scope.data[i]['totalSeasons']) / 60);
          $scope.data[i]['bingeMins'] = ($scope.data[i]['totalEp'] * $scope.data[i]['runtime']) % 60;
-         $scope.data[i]['bingeWeeks'] = Math.floor($scope.data[i]['totalEp'] / 7);
-         $scope.data[i]['bingeDays'] = $scope.data[i]['totalEp'] % 7;
+         $scope.data[i]['bingeWeeks'] = Math.floor(($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) / 7);
+         $scope.data[i]['bingeDays'] = ($scope.data[i]['totalEp']* $scope.data[i]['totalSeasons']) % 7;
        }
      };
    };
@@ -361,7 +362,7 @@ var ModalDemoCtrl = function ($scope, $modal, $log) {
     $scope.items.push(
       this.show.poster,
       this.show.name,
-      this.show.rating,
+      this.show.ratings,
       this.show.totalSeasons,
       this.show.totalEp,
       this.show.runtime,
@@ -369,14 +370,27 @@ var ModalDemoCtrl = function ($scope, $modal, $log) {
       this.show.bingeMins,
       this.show.bingeWeeks,
       this.show.bingeDays,
-      this.show.title
+      this.show.title,
+      this.show.overview
      );
     var modalInstance = $modal.open({
       template: "<div class='modal-header'>"+
-      "<p>" + $scope.items[1] + "</p></div>" +
+      "<h2 class= 'modalTitle'>" + $scope.items[1] + "</h></div>" +
+
       "<div class='modal-body'>"+
+      "<div class='row'>"+
+      "<div class='col-md-6'>"+
       "<img class='modalPoster' src='"+ $scope.items[0] + "'>"+
-      "<p>Summary: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>" +
+      "</div>"+
+
+      "<div class='col-md-6'>"+
+      "<p>Summary: "+ $scope.items[11] +"</p>" +
+
+      "</div>"+
+
+      "</div>"+
+
+      "<div class='container'>"+
       "<p>Rating: " + $scope.items[2] + "</p>" +
       "<p>Total # of Seasons: " + $scope.items[3] + "</p>" +
       "<p>Total # of Episodes: " + $scope.items[4] + "</p>" +
@@ -386,6 +400,7 @@ var ModalDemoCtrl = function ($scope, $modal, $log) {
       "<a href = 'http://www.amazon.com/s?url=search-alias%3Daps&field-keywords=" + $scope.items[10] + "'><img class='modalBrand' src = '../../images/amazon.jpeg'></a>"+
       "<a href = 'http://www.hulu.com/search?q=" + $scope.items[10] + "'><img class='modalBrand' src = '../../images/hulu.jpeg'></a>"+
       "<a href = 'http://www.itunes.com'><img class='modalBrand' src = '../../images/itunes.jpeg'></a></p>"+
+      "</div>" +
       "</div>" +
       "<div class='modal-footer'>"+
       "<button class='btn btn-primary' ng-click='ok()'>CLOSE</button>"+
